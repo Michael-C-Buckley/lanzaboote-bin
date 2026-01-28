45a60b24feb8205e8814811da93527d5a25d3481
Michael Buckley <michaelcbuckley@proton.me>
2026-01-27 20:35:59 -0500

    Lzbt: factored the stub correctly into the derivation after studying the original more

--- packages/lzbt.nix
+++ packages/lzbt.nix
@@ -1,18 +1,59 @@
 {pkgs ? import <nixpkgs> {}}: let
   inherit (pkgs.stdenv) mkDerivation hostPlatform;
+
+  binPath = with pkgs; [
+    binutils-unwrapped
+    sbsigntool
+  ];
+  runtimeLibPath = pkgs.lib.makeLibraryPath (
+    with pkgs; [
+      glibc
+      (lib.getLib stdenv.cc.cc)
+    ]
+  );
+
+  stub = pkgs.callPackage ./stub.nix {inherit pkgs;};
 in
   mkDerivation {
-    pname = "lzbt-bin";
+    pname = "lzbt-systemd-bin";
     version = "v1.0.0";
     src = ./${hostPlatform.system}/lzbt-v1.0.0.tar.gz;
+
+    nativeBuildInputs = [
+      pkgs.patchelf
+      pkgs.makeBinaryWrapper
+    ];
+
+    propagatedBuildInputs = [
+      stub
+      pkgs.sbsigntool
+    ];
 
     dontBuild = true;
     dontConfigure = true;
     dontUnpack = true;
     dontCheck = true;
 
-    installPhase = ''
-      mkdir -p $out
-      tar -xzf $src -C $out
-    '';
+    installPhase =
+      # bash
+      ''
+        mkdir -p $out
+        tar -xzf $src -C $out
+      '';
+
+    postInstall =
+      # bash
+      ''
+        chmod u+w $out/bin/lzbt-systemd
+        patchelf --set-interpreter ${pkgs.stdenv.cc.bintools.dynamicLinker} \
+          --set-rpath ${runtimeLibPath} \
+          $out/bin/lzbt-systemd
+
+        rm -f $out/bin/lzbt
+        makeWrapper $out/bin/lzbt-systemd $out/bin/lzbt \
+          --set PATH ${pkgs.lib.makeBinPath binPath} \
+          --set LANZABOOTE_STUB ${stub}/bin/lanzaboote_stub.efi
+      '';
+
+    meta.mainProgram = "lzbt";
   }
